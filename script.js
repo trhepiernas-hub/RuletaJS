bb9e982cc3be74bf3fe8d2278122f9b3cf385433
Trhe_piernas <hruizdeegino@gmail.com>
2023-12-12 13:54:14 +0100

    proiektua hasi

---
+++ script.js
@@ -0,0 +1,12 @@
+function draw() {
+    const canvas = document.getElementById("canvas");
+    if (canvas.getContext) {
+      const ctx = canvas.getContext("2d");
+  
+      ctx.beginPath();
+      ctx.moveTo(75, 50);
+      ctx.lineTo(100, 75);
+      ctx.lineTo(100, 25);
+      ctx.fill();
+    }
+  }
